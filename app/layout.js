8af7d36636c8f6c0d193ddc37aa5e2fdf13c0f26
MDSOBUJMADBOR <sobujmadbor660@gmail.com>
2026-05-16 13:03:20 +0600

    favicon

--- app/layout.js
+++ app/layout.js
@@ -3,18 +3,14 @@
 import "./globals.css"
 import { ThemeProvider } from "next-themes"
 import LenisProvider from "@/components/lenis-provider"
-import {  Poppins, Inter ,JetBrains_Mono,Smooch_Sans} from "next/font/google";
+import {   Inter,Smooch_Sans} from "next/font/google";
 
 export const metadata = {
   title: "Sobuj Madbor | Web Developer",
   description: "I build modern, responsive websites & web applications.",
 }
 
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["400","500","600","700"]
-})
+
 const inter = Inter({
   variable: "--font-poppins",
   subsets: ["latin"],
@@ -26,12 +22,6 @@ export const smooch = Smooch_Sans({
   weight: ["400","500","600","700"]
 });
 
-
-const mono = JetBrains_Mono({
-  variable: "--font-mono",
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-});
 
 
 
